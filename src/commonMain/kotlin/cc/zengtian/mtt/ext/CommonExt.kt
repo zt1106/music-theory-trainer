b14ac115623f97df65e740c9a93fcf044c3be786
Tony Z <zt1106@gmail.com>
2019-09-12 18:10:17 +0800

    Update CommonExt.kt

--- src/commonMain/kotlin/cc/zengtian/mtt/ext/CommonExt.kt
+++ src/commonMain/kotlin/cc/zengtian/mtt/ext/CommonExt.kt
@@ -3,4 +3,6 @@ package cc.zengtian.mtt.ext
 /**
  * Created by ZengTian on 2019/9/9.
  */
-fun <T : Any> Set<T>.containsNot(t: T) : Boolean = !contains(t)
+fun <T : Any> Set<T>.containsNot(t: T): Boolean = !contains(t)
+
+fun <T : Any> T.asSingletonList(): List<T> = listOf(this)
