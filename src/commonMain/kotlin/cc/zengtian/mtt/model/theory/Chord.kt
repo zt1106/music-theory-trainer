3087c66e766f5aaa247a2a2dc80c53e80ac284c6
Tony Z <zt1106@gmail.com>
2019-09-18 17:51:25 +0800

    Update Chord.kt

--- src/commonMain/kotlin/cc/zengtian/mtt/model/theory/Chord.kt
+++ src/commonMain/kotlin/cc/zengtian/mtt/model/theory/Chord.kt
@@ -1,7 +1,5 @@
 package cc.zengtian.mtt.model.theory
 
-import cc.zengtian.mtt.model.theory.ActualNote.*
-
 /**
  * @param offsets offsets to root note
  */
@@ -33,13 +31,12 @@ open class RelativeChord(offsets: Set<Int>) {
         return intervals.all { steps.contains(it.physicalStep) }
     }
 
-    /**
-     * include itself!!
-     */
     val inversions: List<RelativeChord> by lazy {
         val result = mutableListOf<RelativeChord>()
-        result.add(this)
-        val rootAdded = steps.toMutableList().apply { add(0, 0) }.toList()
+        val rootAdded = steps.toMutableList().apply {
+            sort()
+            add(0, 0)
+        }.toList()
         repeat(steps.size) {
             val idx = it + 1
             val steps = rootAdded.map { step -> step - rootAdded[idx] }.filter { step -> step != 0 }
@@ -50,14 +47,24 @@ open class RelativeChord(offsets: Set<Int>) {
 
     val annotations: List<ChordAnnotation> by lazy {
         val result = mutableListOf<ChordAnnotation>()
-        // no six; suspend; etc
-        ChordType.valuesOfSize(size).forEach { chordType ->
-            inversions.forEachIndexed { inversion, chord ->
+        val inversionsWithItself = inversions.toMutableList().apply { add(0, this@RelativeChord) }
+        // all common chords(defined in ChordType) with their inversions
+        ChordType.values().forEach { chordType ->
+            inversionsWithItself.forEachIndexed { idx, chord ->
                 if (chord.steps == chordType.steps) {
+                    val inversion = if (idx == 0) {
+                        0
+                    } else {
+                        size - idx
+                    }
                     result.add(ChordAnnotation(chordType, inversion))
                 }
             }
         }
+        // TODO suspended, six
+        // minor 7th flat 5
+        // TODO ??
+
         result
     }
 
@@ -81,50 +88,65 @@ open class RelativeChord(offsets: Set<Int>) {
     }
 }
 
-interface ChordAnnotationChecker<T : ChordType> {
-    fun isValid(t: T): Boolean
-}
-
-fun main() {
-    val chord = ActualChord.of(C_, DE, FG, A_)
-    chord.annotations.forEach { println("${it.chordType} ${it.inversion}") }
-    chord.inversions.forEach { println(it) }
-}
-
 open class ActualChord constructor(val actualNote: ActualNote, steps: Set<Int>) : RelativeChord(steps) {
     companion object {
-        fun of(vararg actualNotes: ActualNote): ActualChord {
-            require(actualNotes.size >= 2)
-            val root = actualNotes[0]
-            val offsets = actualNotes.filterIndexed { idx, _ -> idx > 0 }.map { it.getStepsToLeft(root) }.toSet()
-            return ActualChord(root, offsets)
-        }
     }
+
+    constructor(vararg actualNotes: ActualNote) : this(actualNotes[0],
+        actualNotes.filterIndexed { idx, _ -> idx > 0 }.map { it.getStepsToLeft(actualNotes[0]) }.toSet())
+
 }
 
-class Chord private constructor(private val rootNote: Note, steps: Set<Int>) : ActualChord(rootNote.actual, steps) {
-    companion object {
-        fun of(vararg note: Note): Chord {
-            TODO()
-        }
+class Chord private constructor(val root : Note, val others : Set<Note>) : ActualChord(root.actual,
+    others.map { it.actual.getStepsToLeft(root.actual) }.toSet()) {
+
+    val notes = others.toMutableSet().apply { add(root) }.toSet()
+
+    constructor(vararg notes : Note) : this(notes[0], notes.filterIndexed { index, _ -> index > 0 }.toSet())
+
+    init {
+        require(notes.map { it.actual }.toSet().size == others.size + 1)
+    }
+
+    fun beforeInversionRootNote(annotation: ChordAnnotation) : Note {
+        require(annotations.contains(annotation))
+        val beforeActualNote = annotation.getBeforeInversionRootActualNote(root.actual)
+        return notes.find { it.actual == beforeActualNote } ?: throw IllegalStateException()
     }
 }
 
 class ChordAnnotation(val chordType: ChordType,
                       val inversion: Int) {
+    init {
+        require(inversion < chordType.size) {"invalid inversion: $inversion"}
+    }
+
+    val beforeInversionRootStep : Int by lazy {
+        if (inversion == 0) {
+            0
+        }else {
+            chordType.steps.sorted()[inversion - 1]
+        }
+    }
+
+    fun getBeforeInversionRootActualNote(root : ActualNote) : ActualNote = root.getByOffset(-beforeInversionRootStep)
+}
+
+enum class InversionSymbol(val size: Int, val inversion: Int) {
 
 }
 
+// TODO change to normal class so new chord type can be added?
 enum class ChordType(val steps: Set<Int>) {
-    MAJOR_TRIAD(setOf(4, 7)),
-    MINOR_TRIAD(setOf(3, 7)),
-    AUGMENTED_TRIAD(setOf(4, 8)),
-    DIMISHED_TRIAD(setOf(3, 6)),
-    MAJOR_MAJOR_7TH(setOf(4, 7, 11)),
-    DOMINANT_7TH(setOf(4, 7, 10)),
-    MINOR_MAJOR_7TH(setOf(3, 7, 11)),
-    HALF_DIMISHED_7TH(setOf(3, 6, 10)),
-    DIMISHED_7TH(setOf(3, 6, 9));
+    MAJOR_TRIAD(4, 7),
+    MINOR_TRIAD(3, 7),
+    AUGMENTED_TRIAD(4, 8),
+    DIMISHED_TRIAD(3, 6),
+    MAJOR_MAJOR_7TH(4, 7, 11),
+    DOMINANT_7TH(4, 7, 10),
+    MINOR_MAJOR_7TH(3, 7, 11),
+    HALF_DIMISHED_7TH(3, 6, 10),
+    DIMISHED_7TH(3, 6, 9);
 
     companion object {
         val triads: List<ChordType> by lazy { values().filter { it.isTriad() } }
@@ -134,9 +156,11 @@ enum class ChordType(val steps: Set<Int>) {
         }
     }
 
+    constructor(vararg stepsArr: Int) : this(stepsArr.toSet())
+
+    val size : Int by lazy { steps.size + 1 }
+
     fun isTriad(): Boolean = steps.size == 2
 
     fun is7th(): Boolean = steps.size == 3
 }
-
-// TODO dimished 7 special case, annotations size should be 4
