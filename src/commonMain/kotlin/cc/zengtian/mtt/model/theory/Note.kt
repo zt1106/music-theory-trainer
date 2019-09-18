e81832c1e30c6ca06ff2da790b90456bc008214b
Tony Z <zt1106@gmail.com>
2019-09-18 17:51:55 +0800

    Update Note.kt

--- src/commonMain/kotlin/cc/zengtian/mtt/model/theory/Note.kt
+++ src/commonMain/kotlin/cc/zengtian/mtt/model/theory/Note.kt
@@ -16,13 +16,33 @@ class Note private constructor(val actual: ActualNote, val accidental: Accidenta
             }
         }.toMap()
         val ALL_NON_DOUBLE_ACCIDENTAL_NOTES = ALL_NOTES.filterValues { it.accidental.getOffset().absoluteValue < 2 }
+        // common notes
+        val C = of(ActualNote.C_, null)
+        val C_SHARP = of(ActualNote.C_, Accidental.SHARP)
+        val D_FLAT = of(ActualNote.D_, Accidental.FLAT)
+        val D = of(ActualNote.D_, null)
+        val D_SHARP = of(ActualNote.D_, Accidental.SHARP)
+        val E_FLAT = of(ActualNote.E_, Accidental.FLAT)
+        val E = of(ActualNote.E_, null)
+        val F = of(ActualNote.F_, null)
+        val F_SHARP = of(ActualNote.F_, Accidental.SHARP)
+        val G_FLAT = of(ActualNote.G_, Accidental.FLAT)
+        val G = of(ActualNote.G_, null)
+        val G_SHARP = of(ActualNote.G_, Accidental.SHARP)
+        val A_FLAT = of(ActualNote.A_, Accidental.FLAT)
+        val A = of(ActualNote.A_, null)
+        val A_SHARP = of(ActualNote.A_, Accidental.SHARP)
+        val B_FLAT = of(ActualNote.B_, Accidental.FLAT)
+        val B = of(ActualNote.B_, null)
         fun ofActual(actualNote: ActualNote, accidental: Accidental?): Note {
-            return ALL_NOTES[getCompositeKey(actualNote, accidental)] ?: throw IllegalArgumentException("$actualNote $accidental")
+            return ALL_NOTES[getCompositeKey(actualNote, accidental)]
+                ?: throw IllegalArgumentException("$actualNote $accidental")
         }
 
         fun of(actualNote: ActualNote, accidental: Accidental?): Note {
             require(!actualNote.needResolve)
-            return ALL_NOTES[getCompositeKey(actualNote.getByOffset(accidental.getOffset()), accidental)] ?: throw IllegalArgumentException("$actualNote $accidental")
+            return ALL_NOTES[getCompositeKey(actualNote.getByOffset(accidental.getOffset()), accidental)]
+                ?: throw IllegalArgumentException("$actualNote $accidental")
         }
 
         private fun getCompositeKey(actualNote: ActualNote, accidental: Accidental?): Int {
