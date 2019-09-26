cd451a8dae0c9b97d5631f4323d187259b329520
Tony Z <zt1106@gmail.com>
2019-09-26 18:17:12 +0800

    Update Property.kt

--- src/javaFxMain/kotlin/cc/zengtian/mtt/util/Property.kt
+++ src/javaFxMain/kotlin/cc/zengtian/mtt/util/Property.kt
@@ -2,6 +2,7 @@ package cc.zengtian.mtt.util
 
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.beans.value.ObservableValue
 
 /**
  * Created by ZengTian on 2019/9/21.
@@ -12,4 +13,8 @@ fun javafx.beans.property.Property<String>.simpleBind(init: String) {
 
 fun javafx.beans.property.Property<Boolean>.simpleBind(init: Boolean) {
     bind(SimpleBooleanProperty(init))
+}
+
+fun ObservableValue<*>.addSimpleListener(listener: () -> Unit) {
+    addListener { _, _, _ -> listener() }
 }
