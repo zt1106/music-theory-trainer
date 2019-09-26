6c326f3db9f23c98413fc01e3cd5a26b3782f0db
Tony Z <zt1106@gmail.com>
2019-09-26 18:15:46 +0800

    Update Collection.kt

--- src/commonMain/kotlin/cc/zengtian/mtt/util/Collection.kt
+++ src/commonMain/kotlin/cc/zengtian/mtt/util/Collection.kt
@@ -6,3 +6,62 @@ package cc.zengtian.mtt.util
 fun <T : Any> Collection<T>.containsNot(t: T): Boolean = !contains(t)
 
 fun <T : Any> T.asSingletonList(): List<T> = listOf(this)
+
+fun Collection<Long>.median(): Long {
+    return sorted().run {
+        if (size == 0) {
+            return@run 0
+        }
+        if (size % 2 == 0) {
+            (this[size / 2] + this[(size - 1) / 2]) / 2
+        } else {
+            this[(size - 1) / 2]
+        }
+    }
+}
+
+fun List<*>.nextOrFirst(idx: Int): Int {
+    return if (idx < size - 1) {
+        idx + 1
+    } else {
+        0
+    }
+}
+
+fun List<*>.prevOrLast(idx: Int): Int {
+    return if (idx == 0) {
+        size - 1
+    } else {
+        idx - 1
+    }
+}
+
+fun Array<*>.nextOrFirst(idx: Int): Int {
+    return if (idx < size - 1) {
+        idx + 1
+    } else {
+        0
+    }
+}
+
+fun Array<*>.prevOrLast(idx: Int): Int {
+    return if (idx == 0) {
+        size - 1
+    } else {
+        idx - 1
+    }
+}
+
+/**
+ * offset > 0 to right
+ * offset < 0 to left
+ */
+//fun <T> Array<T>.getByOffset(from: Int, offset: Int): T {
+//    require(isNotEmpty())
+//    return if (offset > 0) {
+//        val mod = offset +  % size
+//
+//    } else {
+//
+//    }
+//}
