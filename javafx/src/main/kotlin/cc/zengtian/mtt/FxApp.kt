e29415baa61dff06086f6f0b19d17cdd639f0a53
Tony Z <zt1106@gmail.com>
2019-09-27 18:18:33 +0800

    Update FxApp.kt

--- javafx/src/main/kotlin/cc/zengtian/mtt/FxApp.kt
+++ javafx/src/main/kotlin/cc/zengtian/mtt/FxApp.kt
@@ -25,3 +25,29 @@ class Styles : Stylesheet() {
 //        }
 //    }
 }
+
+package cc.zengtian.mtt;
+
+import cc.zengtian.mtt.ui.QuizTabsView;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import tornadofx.FX;
+
+/**
+ * Created by ZengTian on 9/27/2019.
+ */
+public class JavaFxApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        FX.registerApplication(this, primaryStage);
+        Parent view = FX.find(QuizTabsView.class).getRoot();
+        primaryStage.setScene(new Scene(view, 600, 400));
+        primaryStage.show();
+    }
+}
