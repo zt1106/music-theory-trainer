a61536d2b8383b9ef50c6d3c7108d9e8c2c34ea0
Tony Z <zt1106@gmail.com>
2019-10-12 15:52:17 +0800

    Update ScaleQuizConfig.kt

--- common/src/commonMain/kotlin/cc/zengtian/mtt/config/ScaleQuizConfig.kt
+++ common/src/commonMain/kotlin/cc/zengtian/mtt/config/ScaleQuizConfig.kt
@@ -10,27 +10,28 @@ import kotlinx.serialization.Serializable
  * Created by ZengTian on 2019/9/21.
  */
 @Serializable
-data class ScaleQuizConfig (
+data class ScaleQuizConfig(
     val keys: Set<String> = Key.values().filter {
         val excluded = listOf(Key.C, Key.G_FLAT, Key.C_FLAT, Key.C_SHARP)
         excluded.containsNot(it)
     }.map { it.name }.toSet(),
     val scales: Set<String> = setOf(Scale.MAJOR.name),
-    val questionCount: Int = 0,
+    override val questionCount: Int = 0,
     val answerTypes: Set<ScaleQuestionAnswerType> = setOf(NOTE, NUM, KEY),
     val noteDisplayType: NoteDisplayType = NoteDisplayType.LATIN,
-    val keyDisplayType: KeyDisplayType = KeyDisplayType.LATIN
-) {
+    val keyDisplayType: KeyDisplayType = KeyDisplayType.LATIN,
+    override val timeout: Long = 0
+) : BaseQuizConfig {
     init {
-        require(keys.isNotEmpty()) {"keys can't be empty"}
-        require(scales.isNotEmpty()) {"scales can't be empty"}
-        require(answerTypes.isNotEmpty()) {"answer type can't be empty"}
-        require(questionCount >= 0) {"question count can't be nagetive"}
+        require(keys.isNotEmpty()) { "keys can't be empty" }
+        require(scales.isNotEmpty()) { "scales can't be empty" }
+        require(answerTypes.isNotEmpty()) { "answer type can't be empty" }
+        require(questionCount >= 0) { "question count can't be nagetive" }
         if (keys.size == 1) {
-            require(answerTypes.containsNot(KEY)) {"need at least 2 keys"}
+            require(answerTypes.containsNot(KEY)) { "need at least 2 keys" }
         }
         if (scales.size == 1) {
-            require(answerTypes.containsNot(SCALE)) {"need at least 2 scales"}
+            require(answerTypes.containsNot(SCALE)) { "need at least 2 scales" }
         }
     }
 }
