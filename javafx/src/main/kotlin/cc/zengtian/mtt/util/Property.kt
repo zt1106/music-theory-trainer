6e06f680d1393be1a66aed6c3e2d80647b23246d
Tony Z <zt1106@gmail.com>
2019-10-12 15:52:51 +0800

    Update Property.kt

--- javafx/src/main/kotlin/cc/zengtian/mtt/util/Property.kt
+++ javafx/src/main/kotlin/cc/zengtian/mtt/util/Property.kt
@@ -1,7 +1,11 @@
 package cc.zengtian.mtt.util
 
+import javafx.beans.binding.DoubleBinding
+import javafx.beans.property.DoubleProperty
 import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleDoubleProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.beans.value.ObservableDoubleValue
 import javafx.beans.value.ObservableValue
 
 /**
@@ -17,4 +21,18 @@ fun javafx.beans.property.Property<Boolean>.simpleBind(init: Boolean) {
 
 fun ObservableValue<*>.addSimpleListener(listener: () -> Unit) {
     addListener { _, _, _ -> listener() }
+}
+
+fun minOf(vararg doubleProperties: ObservableDoubleValue) : DoubleProperty {
+    require(doubleProperties.isNotEmpty())
+    val result = SimpleDoubleProperty(doubleProperties.map { it.get() }.min()!!)
+    for (p in doubleProperties) {
+        if (p is DoubleBinding) {
+            println(p.get())
+        }
+        p.addSimpleListener {
+            result.set(doubleProperties.map { it.get() }.min()!!)
+        }
+    }
+    return result
 }
