e54ab4be37974654e46d235d74fc9ddbb2bd6582
Tony Z <zt1106@gmail.com>
2019-10-14 18:12:45 +0800

    Update ScaleNoteQuizController.kt

--- common/src/commonMain/kotlin/cc/zengtian/mtt/controller/ScaleNoteQuizController.kt
+++ common/src/commonMain/kotlin/cc/zengtian/mtt/controller/ScaleNoteQuizController.kt
@@ -2,57 +2,30 @@ package cc.zengtian.mtt.controller
 
 import cc.zengtian.mtt.config.ScaleQuestionAnswerType
 import cc.zengtian.mtt.config.ScaleQuizConfig
+import cc.zengtian.mtt.controller.ScaleNoteQuizController.ScaleQuestionModel
 import cc.zengtian.mtt.theory.Key
 import cc.zengtian.mtt.theory.Note
 import cc.zengtian.mtt.theory.Scale
 import cc.zengtian.mtt.ui.Underscore
 import cc.zengtian.mtt.util.Property
 import cc.zengtian.mtt.util.Storage
-import cc.zengtian.mtt.util.Time
-import cc.zengtian.mtt.util.median
 import kotlin.random.Random
 
 /**
  * Created by ZengTian on 2019/9/22.
  */
-class ScaleNoteQuizController {
+class ScaleNoteQuizController : BaseQuizController<ScaleQuestionModel>() {
 
-    private val config: ScaleQuizConfig by lazy { Storage.getByClassName() ?: ScaleQuizConfig() }
+    override val config: ScaleQuizConfig by lazy { Storage.getByClassName() ?: ScaleQuizConfig() }
 
     private val scales = Scale.builtInValues().filter { config.scales.contains(it.name) }
 
     private val keys = Key.values().filter { config.keys.contains(it.name) }
 
-    val curQuestionProp = Property(generateQuestion())
-    var curQuestion by curQuestionProp
+    val curQuestionProp = MyProperty(generateQuestion())
+    override var curQuestion by curQuestionProp
 
-    val answeredCountProp = Property(0)
-    var answeredCount by answeredCountProp
-        private set
-
-    val correctCountProp = Property(0)
-    var correctCount by correctCountProp
-        private set
-
-    private var timestamp = Time.currentMillisecond()
-
-    private fun resetTimestamp() {
-        timestamp = Time.currentMillisecond()
-    }
-
-    private var usedTimeList: MutableList<Long> = mutableListOf()
-
-    val usedTimeMedianProp = Property<Long>(0)
-    var usedTimeMedian by usedTimeMedianProp
-
-    fun notifyAllProps() {
-        curQuestion = curQuestion
-        answeredCount = answeredCount
-        correctCount = correctCount
-        usedTimeMedian = usedTimeMedian
-    }
-
-    private fun generateQuestion(): ScaleQuestionModel {
+    override fun generateQuestion(): ScaleQuestionModel {
         val scale = scales.random()
         val key = keys.random()
         // exclude 1 because too easy
@@ -63,40 +36,15 @@ class ScaleNoteQuizController {
         return ScaleQuestionModel(num, key, scale, note, type)
     }
 
-    fun answer(answer: Any) {
-        if (!curQuestion.isAnswered()) {
-            answeredCount++
-            curQuestion.answer = answer
-            if (curQuestion.isCorrect()) {
-                correctCount++
-            }
-            usedTimeList.add(Time.currentMillisecond() - timestamp)
-            // TODO performance issue?
-            usedTimeMedian = usedTimeList.median()
-        }
-    }
-
-    /**
-     * @return has next
-     */
-    fun nextQuestion(): Boolean {
-        if (answeredCount == config.questionCount) {
-            return false
-        }
-        curQuestion = generateQuestion()
-        resetTimestamp()
-        return true
-    }
-
     inner class ScaleQuestionModel(
         private val num: Int,
         val key: Key,
         val scale: Scale,
         val note: Note,
         private val answerType: ScaleQuestionAnswerType
-    ) {
+    ) : Question {
         val answerProp = Property<Any?>(null)
-        var answer: Any? by answerProp
+        override var answer: Any? by answerProp
 
         // also exclude 1 because no such questions
         private val numOptions: List<Int> by lazy {
@@ -127,7 +75,7 @@ class ScaleNoteQuizController {
         }
 
         val questionBody: List<Any> by lazy {
-            val numStr = when(num) {
+            val numStr = when (num) {
                 1 -> "1st"
                 2 -> "2nd"
                 3 -> "3rd"
@@ -142,8 +90,11 @@ class ScaleNoteQuizController {
             }
         }
 
-        fun isCorrect() = correctAnswer == answer
-        fun isAnswered() = answer != null
+        override val correct: Boolean
+        get() = correctAnswer == answer
+
+        override val answered: Boolean
+        get() = answer != null
     }
 }
 
