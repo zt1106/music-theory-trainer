798f28a50f9593d2388aaf13d89a593882aaa1c5
Tony Z <zt1106@gmail.com>
2019-10-15 16:38:27 +0800

    Update Question.kt

--- common/src/commonMain/kotlin/cc/zengtian/mtt/controller/Question.kt
+++ common/src/commonMain/kotlin/cc/zengtian/mtt/controller/Question.kt
@@ -1,10 +1,14 @@
 package cc.zengtian.mtt.controller
 
+import cc.zengtian.mtt.util.Property
+
 /**
  * Created by ZengTian on 10/14/2019.
  */
 interface Question {
+    val answerProp: Property<Any?>
     var answer: Any?
+    val correctAnswer: Any
     val correct: Boolean
     val answered: Boolean
 }
