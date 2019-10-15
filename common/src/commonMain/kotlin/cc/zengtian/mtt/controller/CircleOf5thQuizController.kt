5f0b8f7ad5a9c06a70022e16f7d17e95f3449357
Tony Z <zt1106@gmail.com>
2019-10-15 16:38:10 +0800

    Update CircleOf5thQuizController.kt

--- common/src/commonMain/kotlin/cc/zengtian/mtt/controller/CircleOf5thQuizController.kt
+++ common/src/commonMain/kotlin/cc/zengtian/mtt/controller/CircleOf5thQuizController.kt
@@ -2,6 +2,8 @@ package cc.zengtian.mtt.controller
 
 import cc.zengtian.mtt.config.BaseQuizConfig
 import cc.zengtian.mtt.controller.CircleOf5thQuizController.CircleOf5thQuestion
+import cc.zengtian.mtt.util.Property
+
 
 /**
  * Created by ZengTian on 10/14/2019.
@@ -9,6 +11,8 @@ import cc.zengtian.mtt.controller.CircleOf5thQuizController.CircleOf5thQuestion
 class CircleOf5thQuizController : BaseQuizController<CircleOf5thQuestion>(){
 
     override val config: BaseQuizConfig
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val curQuestionProp: MyProperty<CircleOf5thQuestion>
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
     override var curQuestion: CircleOf5thQuestion
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
@@ -19,9 +23,13 @@ class CircleOf5thQuizController : BaseQuizController<CircleOf5thQuestion>(){
     }
 
     inner class CircleOf5thQuestion : Question {
+        override val answerProp: Property<Any?>
+            get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
         override var answer: Any?
             get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
             set(value) {}
+        override val correctAnswer: Any
+            get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
         override val correct: Boolean
             get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
         override val answered: Boolean
